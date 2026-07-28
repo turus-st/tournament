ed7ca24fc88f4eab82fd7fcb6fa63ddeeb11ceba
turus-st <turus@tiscali.it>
2026-07-28 23:08:41 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,44 +1,32 @@
 let currentDay = "venerdi";
 
 async function loadSchedule() {
-
     const response = await fetch("data/matches.json");
     const data = await response.json();
-
     render(data[currentDay]);
 }
 
 function render(fields) {
-
     const schedule = document.getElementById("schedule");
-
     schedule.innerHTML = "";
-
     Object.keys(fields).forEach(field => {
-
         schedule.innerHTML +=
         `
             <div class="field-title">${field}</div>
         `;
-
         fields[field].forEach(match => {
-
             schedule.innerHTML +=
             `
             <div class="match">
-
                 <div class="time">
                     ${match.time}
                 </div>
-
                 <div class="teams">
                     ${match.home} vs ${match.away}
                 </div>
-
                 <div class="score">
                     ${match.score}
                 </div>
-
             </div>
             `;
         });
@@ -46,19 +34,13 @@ function render(fields) {
 }
 
 document.querySelectorAll(".tab").forEach(tab=>{
-
     tab.addEventListener("click", async ()=>{
-
         document
         .querySelectorAll(".tab")
         .forEach(t=>t.classList.remove("active"));
-
         tab.classList.add("active");
-
         currentDay = tab.dataset.day;
-
         loadSchedule();
     });
 });
-
 loadSchedule();
